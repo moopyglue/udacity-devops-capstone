af95ad759091a0473b6f9b9306f4d91389939727
Simon Wall <moopyglue@gmail.com>
2020-10-14 23:18:30 +0100

    worked and basic testing complete - further changes needed to tidy up + increase testing

--- eyes-server/main.go
+++ eyes-server/main.go
@@ -1,16 +1,37 @@
 
+//
+//  todo
+//      - handling different feed info  newgetter/send/etc...
+//      - when hub is disconnected messages build up on the event_queue
+//          consider discard all but last minute and no duplicates
+//      - final test
+//  
+//  after nano
+//      - identify listner by ip address//port or hostname?
+//      - consider epoc time to ensure older messages not transmitted or acted upon
+//      - transmit 'alive' message from hub to each listnere every 5 secs using channels
+//      - on connection with hub a listener should dump it's database to the hub
+//          this will mean that a restarted hub will rebuild it's database from listers
+//          potentially need to 'ask'? new listernbs are empty so easy
+//          special case is when hub restarts and wants all the info, but not duplicates.
+//          maybe the hub responds witha recap??
+//
+
 package main
 
 import (
 	"fmt"
     "bytes"
     "sync"
+    "strings"
     "net"
-    "net/textproto"
+    "io"
+    "bufio"
     "os"
 	"time"
 	"net/http"
 	"github.com/gorilla/websocket"
+    "github.com/timtadh/getopt"
 )
 
 // track latest sender messages
@@ -27,16 +48,19 @@ type link struct {
 }
 var links = make(map[string]link)
 
+var runtype = "none"
+
 // sharing client config
-var share_queue = make(chan string,1000)
+var event_queue = make(chan string,1000)
 // sharing server config
-var dbServ = "localhost:7777"
-var dbPort = ":7777"
-var daemon_chans = make(map[int](chan message))
+var daemon_chans = make(map[int](chan string))
 var daemon_id    = 0;
 
-//simple mux 
-var mux = &sync.Mutex{}
+//simple Read/Write mux 
+var mux = &sync.RWMutex{}
+
+// track active getter list
+var active_getters = make(map[string]string)
 
 // upgrader used to convert https to wss connections
 var upgrader = websocket.Upgrader{
@@ -46,7 +70,6 @@ var upgrader = websocket.Upgrader{
         return true
     },
 }
-
 
 func sendSession(w http.ResponseWriter, r *http.Request) {
 
@@ -62,8 +85,7 @@ func sendSession(w http.ResponseWriter, r *http.Request) {
         return
     }
 
-    crossLink(gid,sid)
-	fmt.Println(logtag,"crossLink sender=",sid," getter=",gid)
+    crossLink(gid,sid,true)
 
 	for {
 		_, rec_mess, err := conn.ReadMessage()
@@ -73,9 +95,7 @@ func sendSession(w http.ResponseWriter, r *http.Request) {
             sendUnlink(sid)
             return
         }
-        saveMessage(sid,rec_mess)
-		fmt.Println(logtag,string(rec_mess))
-		fmt.Println(links)
+        saveMessage(sid,rec_mess,true)
 	}
 }
 
@@ -93,6 +113,11 @@ func getSession(w http.ResponseWriter, r *http.Request) {
         return
     }
 
+    event_queue <- "new_getter,"+id+","
+    mux.Lock()
+    active_getters[id]=""
+    mux.Unlock()
+
     lastv := []byte{}
     nosend := 0
 	for {
@@ -102,7 +127,9 @@ func getSession(w http.ResponseWriter, r *http.Request) {
 	        if err != nil {
                 fmt.Println(logtag,err);
                 fmt.Println(logtag,"exiting")
-                //getUnlink(id)
+                mux.Lock()
+                delete(active_getters,id)
+                mux.Unlock()
                 return
             }
 	        fmt.Println(logtag,string(get_mess))
@@ -115,36 +142,44 @@ func getSession(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func saveMessage(id string,mess []byte) {
+func saveMessage(id string,mess []byte,shareit bool) {
+	fmt.Println("saveMessage: send,"+id+string(mess))
     m := message{value:mess}
     m.mtime=time.Now().Unix()
     mux.Lock()
+    morig := messages[id]
     messages[id]=m
     mux.Unlock()
-    share_queue <- id+","+string(m.value)
+    if string(m.value) != string(morig.value) && shareit {
+        event_queue <- "send,"+id+","+string(m.value)
+    }
 }
 
 func getMessage(gid string) (mess []byte){
-    mux.Lock()
+    mux.RLock()
     if sid,ok := links[gid]; ok {
         if m,ok := messages[sid.value]; ok {
-            mux.Unlock()
+            mux.RUnlock()
             mess = m.value
             return
         }
     }
-    mux.Unlock()
+    mux.RUnlock()
     mess = []byte("NULL")
     return
 }
 
-func crossLink(getter_id string,sender_id string) {
+func crossLink(getter_id string,sender_id string,shareit bool) {
     if( getter_id != "" ) {
+	    fmt.Println("crossLink link,"+getter_id+","+sender_id)
         s := link{value:sender_id}
         s.mtime=time.Now().Unix()
         mux.Lock()
         links[getter_id] = s
         mux.Unlock()
+        if shareit {
+            event_queue <- "link,"+getter_id+","+sender_id
+        }
     }
 }
 
@@ -154,92 +189,131 @@ func sendUnlink(gid string) {
     mux.Unlock()
 }
 
-//func getUnlink(gid string) {
-//    mux.Lock()
-//    delete(links,gid)
-//    mux.Unlock()
-//}
-
-func stats_out() {
+func monitor() {
     for {
-        time.Sleep(2000*time.Millisecond);
-        fmt.Println("share_queue",len(share_queue))
+        time.Sleep(60000*time.Millisecond);
+        fmt.Println("event_queue",len(event_queue),"daemon_chans",len(daemon_chans));
     }
 }
 
-func share_client() {
+func share_client(dbServ string) {
 
     sec :=1000*time.Millisecond;
 
     // infinited loop to attempt reconnectying every sleep_ms 
     // normal operation is to stay connected but loop enables recovery
-    for {
 
-        // generate remote TCP Address
-        tcpAddr, err := net.ResolveTCPAddr("tcp4", dbServ )
-        if err != nil {
-            fmt.Println("share_client: ResolveTCPAddr:", err.Error())
-	        time.Sleep(5*sec)
-            continue
+    if runtype == "standalone" {
+        for {
+            <-event_queue
         }
+        // just consume queue thrwoing it away when in stadnalone mode
+    }
+
+    // to ensure that at least one event is sent every 5 secs
+    // (resulting in network traffic, which ensures network
+    // remains tested and operational)
+    go func() {
+        for {
+            time.Sleep(5*sec)
+            if len(event_queue) == 0  {
+                // chan len is used to avoid pointless event build up when
+                // no network connection.
+                event_queue <- ""
+            }
+        }
+    }()
+
+    // main loop, includes making connection to server so that if connection lost
+    // the will attempt reconnection every 5 seconds, under normal operation we
+    // would hoe the loop would only be executed once.
+    for {
 
         // give daemon chance to start if started at same time
 	    time.Sleep(2*sec);
 
         // connect to server port
-        conn, err := Dial("tcp", tcpAddr)
+        c, err := net.Dial("tcp", dbServ)
         if err != nil {
             fmt.Println("share_client: Dial failed:", err.Error())
-	        time.Sleep(5*sec);
+	        time.Sleep(3*sec);
             continue
         }
         fmt.Println("share_client: connected to", dbServ )
 
         stop := make(chan bool)
 
-        // go routine loop where we share data sent via 'share_queue' with remote database
+        // at start of a new connection share a list of all the getter/setter links
+        // if just started this will be empty but if because we have lost hub the
+        // refills the hub with info it neads about this client connection
+        bootstring := ""
+        mux.RLock()
+        for  i := range active_getters {
+            if _, ok := links[i] ; ok {
+                bootstring = bootstring + "ag," + i + "," + links[i].value + "\n"
+            }
+        }
+        mux.RUnlock()
+        _, err = io.WriteString(c,bootstring)
+        if err != nil {
+            fmt.Println("share_client: failed writing boot string to hub:", err.Error())
+            c.Close()
+            continue
+        }
+
+        // go routine loop where we share data sent via 'event_queue' with remote database
         go func() {
             fmt.Println("share_client: waiting on new messages")
             for {
 
                 s:=""
                 select {
-                    case s = <-share_queue:
-                        fmt.Println("share_client: received message")
+                    case s = <-event_queue:
                     case <-stop:
-                        fmt.Println("share_client: Writer recieved stop request")
                         break
                 }
 
-                _, err = conn.Write( []byte(s+"\n") )
+                _, err = io.WriteString(c,s+"\n")
                 if err != nil {
                     fmt.Println("share_client: Write to server failed:", err.Error())
                     break
                 }
             }
 
-            conn.Close()
+            c.Close()
             stop <- true
         }()
 
-        // go routine loop where we share data sent via 'share_queue' with remote database
+        // go routine loop where we share data sent via 'event_queue' with remote database
         go func() {
 
-            // read single byte  
-            // read remaining bytes
-            // store message with locking 
-            // loop
-            fmt.Println("Listner recived >stop<")
+            input := bufio.NewScanner(c)
+            for input.Scan() {
+                a := strings.SplitN(input.Text(),",",3)
+                if len(a) != 3 {
+                    continue
+                }
+                if a[0] == "send" {
+                    saveMessage(a[1],[]byte(a[2]),false)
+                } else if a[0] == "link" {
+                    crossLink(a[1],a[2],false)
+                }
+                fmt.Println("share_client: Read:",input.Text())
+            }
 
+            c.Close()
+            stop <- true
         }()
 
-        <- stop
+        <-stop
+        <-stop
+        fmt.Println("share_client: stopped")
 
     }
 
 }
 
-func share_daemon() {
+func share_daemon(dbPort string) {
 
     //tcpAddr, err := net.ResolveTCPAddr("tcp4",dbPort)
     //if err != nil {
@@ -251,9 +325,9 @@ func share_daemon() {
     listener, err := net.Listen("tcp4",dbPort)
     if err != nil {
         fmt.Println("share_daemon: Listen:", err.Error())
-        os.Exit(31)
+        select { }
     }
-    fmt.Println("share_daemon: Listen:",listener)
+    fmt.Println("share_daemon: Listen now active:",listener)
 
     // accept connections from share clients
     // we will 
@@ -264,7 +338,7 @@ func share_daemon() {
                 fmt.Println("share_daemon: Listener.Accept():", err.Error())
                 os.Exit(32)
             }
-            fmt.Println("share_daemon: ListenTCP:",listener)
+            fmt.Println("share_daemon: Accept:",listener)
             go share_daemon_peer(conn)
         }
     }()
@@ -275,69 +349,147 @@ func share_daemon_peer(c net.Conn) {
 
     // uniquely identify this client connection 
     // and create a channel to listen on for 
-    peerChan := make(chan message,1000)
+    peerChan := make(chan string,1000)
     mux.Lock()
     daemon_id = daemon_id + 1
     peerID := daemon_id
     daemon_chans[peerID] = peerChan
     mux.Unlock()
 
+    stop := make(chan bool)
+
     // go routine to listen for notifications on newly created channel
     go func() {
         fmt.Println("share_daemon_peer: listening on peerChan:",peerID)
         for {
-            m := <- peerChan
-            fmt.Println("peerID",peerID,"message",m,"NEED ACTION TO BE CODED");
+            s:=""
+            select {
+                case s = <-peerChan:
+                case <-stop:
+                    break
+                }
+
+            _, err := io.WriteString(c,s+"\n")
+            if err != nil {
+                fmt.Println("share_client: Write to server failed:", err.Error())
+                break
+            }
         }
+        c.Close()
+        stop <- true
     }()
 
     // go routine to recived client messages and share with other registered clients 
     go func() {
         fmt.Println("share_daemon_peer: waiting on Network input:",peerID)
-        for {
-            n, err := c.Read(b)
-            if (err != nil ) {
-                fmt.Println("share_daemon_peer: netwrok read:",peerID,":Read failed:", err.Error())
-                break
-            }
-            fmt.Println("share_daemon_peer: processing:",n,b)
-            if (n>0) {
-                cached := append(cached,b...)
-                for {
-                    mc := int(cached[0])
-                    if( len(cached) > mc ) {
-                        fmt.Println("share_daemon_peer: netwrok read:",peerID,mc,":",string(cached[1:mc]))
-                        cached = cached[mc:]
-                    } else  {
-                        break
+        input := bufio.NewScanner(c)
+        for input.Scan() {
+            if input.Text() != "" {
+                a := strings.SplitN(input.Text(),",",3)
+                if len(a) != 3 {
+                    fmt.Println("share_daemon_peer: BAD COMS(1):",input.Text())
+                    continue
+                }
+                if a[0] == "new_getter" {
+                    // if a new getter has a link stored on the hub share 
+                    // back to the connected carrier
+                    mux.RLock()
+                    if v, ok := links[a[1]] ; ok {
+                        peerChan <- "link,"+a[1]+","+v.value
+                    }
+                    mux.RUnlock()
+                    continue
+                } else if a[0] == "ag" {
+                    // record 'ag' (activate getters) but don't pass them on
+                    // to other carriers
+                    crossLink(a[1],a[2],false)
+                    continue
+                } else if a[0] == "link" {
+                    // record the link information and let it drop through to be
+                    // passed out to all other carriers
+                    crossLink(a[1],a[2],false)
+                } else if a[0] != "send" {
+                    // if not send that not a known request so marked as bad
+                    // otherwise just let it be passed to other carriers
+                    fmt.Println("share_daemon_peer: BAD COMS(2):",input.Text())
+                    continue
+                }
+                mux.RLock()
+                for i, xchan := range daemon_chans {
+                if i != peerID {
+                        xchan <- input.Text()
                     }
                 }
+                mux.RUnlock()
             }
         }
+        c.Close()
+        stop <- true
     }()
 
-    select { }
+    <- stop
+    <- stop
+    fmt.Println("share_daemon_peer: closed peer:",peerID)
 }
 
 func main() {
 
-    for _, pair := range os.Environ() {
-        fmt.Println(pair)
+	short := "l:m:h:"
+	long := []string{ "listen","manager","hub" }
+
+	listenPort := "6001"
+    managerPort := "7001"
+    hubURL := "localhost:7001"
+
+    otherargs, optargs, err := getopt.GetOpt(os.Args[1:], short, long)
+	for _, oa := range optargs {
+		switch oa.Opt() {
+		case "-l", "--listen":
+			listenPort = oa.Arg()
+		case "-m", "--manager":
+			managerPort = oa.Arg()
+		case "-h", "--hub":
+			hubURL = oa.Arg()
+		}
+	}
+
+    if len(otherargs) > 0 {
+        runtype = otherargs[0]
+    } else {
+        runtype = "standalone"
+    }
+    if runtype == "hub"  {
+	    fmt.Println("Hub Port:",managerPort);
+        go share_daemon(":"+managerPort)
+    } else if runtype == "carrier" {
+	    fmt.Println("Listening on Port:",listenPort);
+	    fmt.Println("  Manager on Port:",managerPort);
+	    fmt.Println("     Hub Location:",hubURL);
+        go share_client(hubURL)
+        go share_daemon(":"+managerPort)
+    } else if runtype == "standalone" {
+	    fmt.Println("Listening on Port:",listenPort);
+        go share_client(hubURL)
+    } else {
+	    fmt.Println("need to specify a run type of hub, carrier, or standalone");
+        os.Exit(1)
     }
 
-    // 
-    go stats_out()
-    go share_client()
-    go share_daemon()
+    // to be used for background monitoring
+    go monitor()
 
-	http.HandleFunc("/send", sendSession)
-	http.HandleFunc("/get", getSession)
-	http.Handle("/", http.FileServer(http.Dir("/app/html/")))
+    if runtype == "hub" {
+        select { } // pause and just run daemon only
+    } else {
+	    http.HandleFunc("/send", sendSession)
+	    http.HandleFunc("/get", getSession)
+	    http.Handle("/", http.FileServer(http.Dir("./html/")))
 
-	fmt.Printf("Starting \n");
-	err := http.ListenAndServe(":"+os.Args[1], nil)
-	if err != nil {
-		panic("ListenAndServe: " + err.Error())
-	}
+	    fmt.Printf("Starting \n");
+	    err2 := http.ListenAndServe(":"+listenPort, nil)
+	    if err2 != nil {
+		    panic("ListenAndServe: " + err.Error())
+	    }
+    }
 }
 
